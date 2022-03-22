4a865719a53e525407e3e464535af5f7aa649c9f
Cristian Florez <69540787+devflorez@users.noreply.github.com>
2022-03-21 20:40:35 -0500

    seo y favicon agregado

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,23 @@
-
-
-import React from 'react'
-
+import React from "react";
+import Head from "next/head";
 export default function Layout({ children }) {
   return (
-    <div className='layout'>
-      <main>
-      {children}
-      </main>
+    <div className="layout">
+      <Head>
+        <title>Adminsitrador CRUD 🧑‍🍳</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Este pequeño aplicativo de un CRUD 🧑‍🍳, enfocado a un restaurante el cual utiliza NextJS"
+        />
+        <meta
+          name="keywords"
+          content="desarrollo, react, nextjs, crud, restaurante, administrador, devflorez, ingenieroflorez, cristian david florez merlano"
+        />
+      </Head>
+
+      <main>{children}</main>
     </div>
-  )
+  );
 }
